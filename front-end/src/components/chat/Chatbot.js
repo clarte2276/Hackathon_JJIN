e04742927900d354ad329f32b3840c7ec62a745a
on22y <lucy14lee@gmail.com>
2024-07-21 23:53:36 +0900

    채팅아이콘작게함용

--- front-end/src/components/chat/Chatbot.js
+++ front-end/src/components/chat/Chatbot.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect, useRef } from "react";
-import io from "socket.io-client";
-import "./Chatbot.css";
-import chatbotImg from "../images/chatbotImg.png";
-import sendBtn from "../images/sendBtn.png";
+import React, { useState, useEffect, useRef } from 'react';
+import io from 'socket.io-client';
+import './Chatbot.css';
+import chatbotImg from '../images/chatbotImg.png';
+import sendBtn from '../images/sendBtn.png';
 
 const socket = io();
-const aiuser = "내꿈코";
+const aiuser = '내꿈코';
 
 const Chatbot = ({ currentUser }) => {
   const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
-    socket.emit("join room", { user: currentUser });
+    socket.emit('join room', { user: currentUser });
 
-    socket.on("init messages", (msgs) => {
+    socket.on('init messages', (msgs) => {
       setMessages(msgs);
       scrollToBottom();
     });
 
-    socket.on("chat message", (msg) => {
-      console.log("Received message: ", msg);
+    socket.on('chat message', (msg) => {
+      console.log('Received message: ', msg);
       setMessages((prevMessages) => [...prevMessages, msg]);
     });
 
-    socket.on("gpt response", (msg) => {
-      console.log("Received GPT response: ", msg);
+    socket.on('gpt response', (msg) => {
+      console.log('Received GPT response: ', msg);
       setMessages((prevMessages) => [...prevMessages, msg]);
     });
 
     return () => {
-      socket.off("chat message");
-      socket.off("init messages");
-      socket.off("gpt response");
+      socket.off('chat message');
+      socket.off('init messages');
+      socket.off('gpt response');
     };
   }, [currentUser]);
 
@@ -45,56 +45,56 @@ const Chatbot = ({ currentUser }) => {
     e.preventDefault();
     if (input) {
       // 사용자 메시지 전송
-      socket.emit("chat message", { text: input, user: currentUser });
+      socket.emit('chat message', { text: input, user: currentUser });
 
       try {
-        const response = await fetch("/ask-gpt4", {
-          method: "POST",
+        const response = await fetch('/ask-gpt4', {
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({ input }),
         });
         const data = await response.json();
-        console.log("Server response: ", data);
+        console.log('Server response: ', data);
 
         // GPT 응답을 내꿈코로 전송
-        socket.emit("gpt response", {
+        socket.emit('gpt response', {
           text: data.response,
           user: aiuser,
         });
 
-        setInput("");
+        setInput('');
       } catch (error) {
-        console.error("Error:", error);
+        console.error('Error:', error);
       }
     }
   };
 
   const handleButtonClick = (label, text) => {
-    socket.emit("chat message", { text, user: currentUser });
-    socket.emit("ask chatbot", { label });
+    socket.emit('chat message', { text, user: currentUser });
+    socket.emit('ask chatbot', { label });
   };
 
   const handleSeatsButtonClick = async () => {
-    const userText = "현재 예약 가능한 좌석 현황을 알려주라";
+    const userText = '현재 예약 가능한 좌석 현황을 알려주라';
     // 사용자 메시지 전송
-    socket.emit("chat message", { text: userText, user: currentUser });
+    socket.emit('chat message', { text: userText, user: currentUser });
 
     try {
-      const response = await fetch("/api/empty");
+      const response = await fetch('/api/empty');
       const data = await response.json();
       const availableSeats = data.availableSeats; // 수정된 부분
       const text = `현재 예약 가능한 좌석은 ${availableSeats}개입니다.`;
       // 내꿈코 메시지로 전송
-      socket.emit("gpt response", { text, user: aiuser });
+      socket.emit('gpt response', { text, user: aiuser });
     } catch (error) {
-      console.error("Error fetching empty seats:", error);
+      console.error('Error fetching empty seats:', error);
     }
   };
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
@@ -112,17 +112,10 @@ const Chatbot = ({ currentUser }) => {
           {messages.map((msg, index) => (
             <li
               key={index}
-              className={
-                msg.user === aiuser
-                  ? "chatbot-message-left-unique"
-                  : "chatbot-message-right-unique"
-              }
+              className={msg.user === aiuser ? 'chatbot-message-left-unique' : 'chatbot-message-right-unique'}
             >
               <div className="message-content">
-                <strong className={msg.user === aiuser ? "nickname" : ""}>
-                  {msg.user}:
-                </strong>{" "}
-                {msg.text}
+                <strong className={msg.user === aiuser ? 'nickname' : ''}>{msg.user}:</strong> {msg.text}
               </div>
             </li>
           ))}
@@ -131,53 +124,33 @@ const Chatbot = ({ currentUser }) => {
       </div>
       <div className="chatbot-button-group-unique">
         <div className="chatbot-button-row">
-          <button
-            onClick={() =>
-              handleButtonClick(
-                "TEXT 1",
-                "나의 공강을 책임질 빈백의 위치를 알려줘!"
-              )
-            }
-          >
+          <button onClick={() => handleButtonClick('TEXT 1', '나의 공강을 책임질 빈백의 위치를 알려줘!')}>
             나의 공강을 책임질 빈백의 위치를 알려줘!
           </button>
-          <button
-            onClick={() =>
-              handleButtonClick("TEXT 2", "빈백 운영시간은 어떻게 돼?")
-            }
-          >
+          <button onClick={() => handleButtonClick('TEXT 2', '빈백 운영시간은 어떻게 돼?')}>
             빈백 운영시간은 어떻게 돼?
           </button>
         </div>
         <div className="chatbot-button-row">
-          <button
-            onClick={() =>
-              handleButtonClick(
-                "TEXT 3",
-                "나의 완벽한 숙면을 위해 음악을 추천해줄래?"
-              )
-            }
-          >
+          <button onClick={() => handleButtonClick('TEXT 3', '나의 완벽한 숙면을 위해 음악을 추천해줄래?')}>
             나의 완벽한 숙면을 위해 음악을 추천해줄래?
           </button>
-          <button onClick={handleSeatsButtonClick}>
-            현재 예약 가능한 좌석 현황을 알려주라
-          </button>
+          <button onClick={handleSeatsButtonClick}>현재 예약 가능한 좌석 현황을 알려주라</button>
         </div>
-        <form id="chatbot-chatform-unique" onSubmit={handleSubmit}>
-          <input
-            id="chatbot-messageinput-unique"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            autoComplete="off"
-          />
-          <button type="submit">
-            <div className="sendBtn">
-              <img src={sendBtn} alt="전송 아이콘"></img>
-            </div>
-          </button>
-        </form>
       </div>
+      <form id="chatbot-chatform-unique" onSubmit={handleSubmit}>
+        <input
+          id="chatbot-messageinput-unique"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          autoComplete="off"
+        />
+        <button type="submit">
+          <div className="sendBtn">
+            <img src={sendBtn} alt="전송 아이콘"></img>
+          </div>
+        </button>
+      </form>
     </div>
   );
 };
