1f983e938dac9b3cabc4307bc3b507e6f975a130
on22y <lucy14lee@gmail.com>
2024-07-22 06:36:41 +0900

    header img 로 변경ㅋ

--- front-end/src/components/home/Header.js
+++ front-end/src/components/home/Header.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import mainImg from '../images/mainImg.png';
 import './Header.css';
+import HeaderImg from '../images/header.png';
 
 function Header() {
   return (
     <div className="headerAll">
-      <div className="headerText">
-        <p className="text1">
-          <strong>welcome to</strong>
-        </p>
-        <h1>DreamBAG</h1>
-        <p className="text2">동국대학생의 쉼을 꿈꾸는 드림백</p>
-      </div>
-      <div className="headerImg">
-        <img className="mainImg" src={mainImg} />
-      </div>
+      <img className="HeaderImg" src={HeaderImg} />
     </div>
   );
 }
