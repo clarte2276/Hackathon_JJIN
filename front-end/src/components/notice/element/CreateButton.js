8be837979bd5b53a9c8e9bebba267e4d6362f5b8
eae22 <eun0110@dgu.ac.kr>
2024-07-21 23:28:25 +0900

    건의사항 글작성 새글작성 고침

--- front-end/src/components/notice/element/CreateButton.js
+++ front-end/src/components/notice/element/CreateButton.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Element.css';
 
 function CreateButton({ nextNo }) {
+  const location = useLocation();
+
+  let newPostPath;
+  if (location.pathname === '/notice') {
+    newPostPath = '/notice/process/new_Post';
+  } else if (location.pathname === '/suggest') {
+    newPostPath = '/suggest/process/new_Post';
+  }
+
   return (
     <div className="CreateButton_layout">
-      <Link className="CreateButton" to={`/notice/process/new_Post`} state={{ nextNo }}>
+      <Link className="CreateButton" to={newPostPath} state={{ nextNo }}>
         새 글 작성
       </Link>
     </div>
