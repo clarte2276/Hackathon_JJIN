8809471f0f35bf0b7d249a83c0ce7e09f7120daf
on22y <lucy14lee@gmail.com>
2024-07-21 16:31:02 +0900

    예약 폼 아직 수정중, 홈 화면 생성

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
 import Login from './components/mypage/Login';
 import Signup from './components/mypage/Signup';
 import Mypage from './components/mypage/Mypage';
 import Chatbot from './components/chat/Chatbot';
+import Reservation from './components/form/Reservation';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/loginpage" element={<Login />} />
         <Route path="/signuppage" element={<Signup />} />
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/Chatbot" element={<Chatbot />} />
+        <Route path="/bag/form" element={<Reservation />} />
       </Routes>
     </BrowserRouter>
   );
