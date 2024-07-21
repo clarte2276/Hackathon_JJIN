41c2dbda800b3da512b246608adfba0f314a2f62
jei1126 <jei1126@dgu.ac.kr>
2024-07-22 07:49:47 +0900

    스크롤수정

--- front-end/src/components/notice/element/CreateButton.js
+++ front-end/src/components/notice/element/CreateButton.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './Element.css';
 
 function CreateButton({ nextNo }) {
+  const navigate = useNavigate();
   const location = useLocation();
 
-  let newPostPath;
-  if (location.pathname === '/notice') {
-    newPostPath = '/notice/process/new_Post';
-  } else if (location.pathname === '/suggest') {
-    newPostPath = '/suggest/process/new_Post';
-  }
+  const handleCreateClick = () => {
+    window.scrollTo(0, 0); // 페이지 상단으로 스크롤
+    let newPostPath;
+    if (location.pathname === '/notice') {
+      newPostPath = '/notice/process/new_Post';
+    } else if (location.pathname === '/suggest') {
+      newPostPath = '/suggest/process/new_Post';
+    }
+    navigate(newPostPath, { state: { nextNo } });
+  };
 
   return (
     <div className="CreateButton_layout">
-      <Link className="CreateButton" to={newPostPath} state={{ nextNo }}>
+      <button className="CreateButton" onClick={handleCreateClick}>
         새 글 작성
-      </Link>
+      </button>
     </div>
   );
 }
